a796150f8d38472b89e3087aeaa42458b2c5e247
paipradeep <pradeep.pai@phoenix.edu>
2023-02-05 07:31:23 -0700

    repeat string

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(str,num) {
+    let finalstr= "";
+    if (num <0) {
+        finalstr = "ERROR";
+    }
+    for (let i=1; i<=num; i++){
+        finalstr=finalstr.concat(str);
+    }
+    return finalstr;
 };
 
 // Do not edit below this line
