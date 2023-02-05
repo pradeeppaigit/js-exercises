90567d46ee8a0d006f4a2422ec69683bcb7c935c
paipradeep <pradeep.pai@phoenix.edu>
2023-02-05 07:40:54 -0700

    reverse string

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,14 @@
-const reverseString = function() {
+const reverseString = function(str) {
+    let splitStr = str.split("");
+    //Splits the string into an array
+
+    let reverseArray = splitStr.reverse();
+    //Reverses the array
+
+    let joinArray = reverseArray.join("");
+    //Join the array
+
+    return joinArray;
 
 };
 
